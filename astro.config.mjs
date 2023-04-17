1e0071b11ef1e8d26b6c64166bfaa40919536d7f
Rowin Hernandez <rowinkaleb@gmail.com>
2023-04-17 21:57:46 +0100

    Add Svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import lit from "@astrojs/lit";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), lit()]
+  integrations: [tailwind(), lit(), svelte()]
 });
