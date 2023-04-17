f3d5f411bbce6537926bf48ad37b9ac7924008e8
Rowin Hernandez <rowinkaleb@gmail.com>
2023-04-17 21:58:48 +0100

    Add solid

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import lit from "@astrojs/lit";
-
 import svelte from "@astrojs/svelte";
+
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), lit(), svelte()]
+  integrations: [tailwind(), lit(), svelte(), solidJs()]
 });
