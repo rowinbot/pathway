4606412e80ea593b5130e0a63455953fdaeb25b6
Rowin Hernandez <rowinkaleb@gmail.com>
2023-05-04 16:44:12 +0100

    Use custom postcss config to add plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import lit from "@astrojs/lit";
 import svelte from "@astrojs/svelte";
 import solidJs from "@astrojs/solid-js";
 
 import node from "@astrojs/node";
+import autoprefixer from "autoprefixer";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), lit(), svelte(), solidJs()],
+  integrations: [lit(), svelte(), solidJs()],
   output: "server",
   adapter: node({
-    mode: "standalone"
-  })
+    mode: "middleware",
+  }),
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Assistant", ...defaultTheme.fontFamily.sans],
+        sans: ["Assistant"],
       },
     },
   },
