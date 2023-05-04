b9f5330f43a8568ce21146e96b6e4ac41d4abc22
Rowin Hernandez <rowinkaleb@gmail.com>
2023-05-04 15:49:19 +0100

    Layout + structural changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import lit from "@astrojs/lit";
 import svelte from "@astrojs/svelte";
-
 import solidJs from "@astrojs/solid-js";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), lit(), svelte(), solidJs()],
   output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
