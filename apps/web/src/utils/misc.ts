99139961e158f0a7532f23064b6cb97332e07d85
Rowin Hernandez <rowinkaleb@gmail.com>
2023-05-20 11:48:41 +0100

    Return value from wrapped function on debounce

--- apps/web/src/utils/misc.ts
+++ apps/web/src/utils/misc.ts
@@ -1,15 +1,22 @@
-export function debounce<A extends unknown[]>(
-  fn: (...args: A) => void,
+export function debounce<A extends unknown[], R extends unknown>(
+  fn: (...args: A) => R | Promise<R>,
   delay: number
 ) {
   let timeoutId: ReturnType<typeof setTimeout> | null = null;
 
-  return function (...args: A) {
-    if (timeoutId) {
-      clearTimeout(timeoutId);
-      timeoutId = null;
-    }
+  return function (...args: A): Promise<R> {
+    return new Promise<R>((resolve) => {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+        timeoutId = null;
+      }
 
-    timeoutId = setTimeout(() => fn(...args), delay);
+      timeoutId = setTimeout(() => {
+        const value = fn(...args);
+
+        if (value instanceof Promise) value.then(resolve);
+        else resolve(value);
+      }, delay);
+    });
   };
 }
